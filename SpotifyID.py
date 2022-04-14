7f847ca205d48b6d5331f38b5a25b4edd6b54e18
Graham Morrison <92757624+GrahamMo@users.noreply.github.com>
2022-04-14 03:08:57 -0400

    Delete SpotifyID.py

--- SpotifyID.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ["ClientID"] = "dad707118d1342abb51852540c3a0cbd"
-os.environ["ClientSecret"] = "f8416b468a0944eda7dc057937fead08"
-os.environ["RedirectURL"] = "http://127.0.0.01:8080"
